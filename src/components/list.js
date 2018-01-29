c8f0ef5f8532d3080a8b971e099a5190e8873f6f
FranceBe <france.berut@gmail.com>
2018-01-29 17:17:10 +0100

    Ajout d'une case à cocher et d'un bouton traiter pour changer le statut de plusieurs elements en même temps

--- src/components/list.js
+++ src/components/list.js
@@ -2,18 +2,56 @@ import React, {Component} from 'react';
 import TodoItem from './todoItem.jsx';
 
 class List extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            selectedTodos: []
+        };
+        this.processTodos = this.processTodos.bind(this);
+        this.addToList = this.addToList.bind(this);
+        this.toggleTodo = this.toggleTodo.bind(this);
+}
     render() {
+        const afficher = this.state.selectedTodos.length > 0;
         return(
             <div className="liste">
-                <div className="NbrTodo">To-do : [ <span className="TodoLength">{this.props.todos.length} </span> ] </div>
-                <div className="Explication"> Changez le statut de l'élément en cliquant sur le bouton associé. </div>
+                <div className="NbrTodo">To-do : [ <span className="TodoLength">{this.props.todos.length} </span> ]</div>
+                <div className="boutonTraiter"> { afficher ? <button onClick={this.processTodos}>Traiter</button> : null } </div>
+                <div className="Explication"> Changez le statut de l'élément en cliquant sur le bouton </div>
                 <div className="EltAdded">{this.showTodos(this.props.todos)}</div>
-        </div>
+            </div>
         );
     }
+
+processTodos(){
+    let list = this.state.selectedTodos;
+    list.forEach( item => item.done = !item.done);
+    this.setState({ selectedTodos: list})
+}
+
     toggleTodo(todo, index){
         this.props.onTodoToggle(todo, index);
     }
+
+addToList(index, doAdd){
+    let _todo = this.props.todos[index];
+    if(doAdd){
+        if(this.state.selectedTodos.indexOf(_todo) === -1){
+            this.setState({
+                selectedTodos : this.state.selectedTodos.concat(_todo)
+            });
+        }
+    }
+    else {
+        let _list = this.state.selectedTodos;
+        _list.splice(index, 1);
+        this.setState({
+            selectedTodos: _list
+        })
+    }
+}
+
+
     showTodos(todos){
         return(
             todos.map((todo, idx) => {
@@ -21,7 +59,8 @@ class List extends Component {
                 <TodoItem item={todo}
                           key={todo.title+''+idx}
                           idx={idx}
-                          toggleTodo={this.toggleTodo.bind(this)}/>
+                          addToList={this.addToList}
+                          toggleTodo={this.toggleTodo}/>
                 )
             })
         );
--- src/components/todoItem.jsx
+++ src/components/todoItem.jsx
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 
 class TodoItem extends Component {
+
     render(){
         const todo = this.props.item;
         const idx = this.props.idx;
         let str = todo.done ? 'Fait ' : 'A faire '
         return(
             <div className="todo" key="todo-{todo.title}">
+                <input type="checkbox"
+                    value={idx}
+                    onClick={(e) => this.props.addToList(idx, e.target.checked)}/>
                 {todo.title}
                 <button onClick={() => this.props.toggleTodo(todo, idx)}>
                     {str}
